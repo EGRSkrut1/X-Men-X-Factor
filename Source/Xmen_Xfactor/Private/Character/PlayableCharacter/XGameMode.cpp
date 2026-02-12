14bd6efeb46529d424ec61f7695ec65cee5a65d5
Egor <EGRSKRUT@gmail.com>
2026-02-12 22:51:43 +0700

    feat: port grid system and character refactor from `characters` branch (78dd3f09)
    Apply core changes from characters branch:
    - Staggered hex grid with cell lookup by index/location
    - X-prefix character class refactor (Exploration/Tactical separation)
    - Grid data structures and visualization components
    - Main menu foundation and asset reorganization
    
    This ports the complete grid system foundation and character restructuring
    from the characters branch, replacing the previous template implementation.

---
+++ Source/Xmen_Xfactor/Private/Character/PlayableCharacter/XGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Character/PlayableCharacter/XGameMode.h"
+
+AXGameMode::AXGameMode()
+{
+
+}
---
+++ Source/Xmen_Xfactor/Public/Character/ExplorationCharacter/XEmmaFrostExplorationCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/ExplorationCharacter/XExplorationCharacter.h"
+#include "XEmmaFrostExplorationCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class XMEN_XFACTOR_API AXEmmaFrostExplorationCharacter : public AXExplorationCharacter
+{
+	GENERATED_BODY()
+	
+};
